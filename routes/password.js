692335864a971088bb7052186711f008752cad1e
Sumit-singh-tomar <142825774+Sumit-singh-tomar@users.noreply.github.com>
2024-02-27 21:40:21 +0530

    Update password.js

--- routes/password.js
+++ routes/password.js
@@ -2,7 +2,7 @@ const express = require('express')
 const SibApiV3Sdk = require('sib-api-v3-sdk')
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 var apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = "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"
+
 
 const router = express.Router()
 
